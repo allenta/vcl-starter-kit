ab513e0bb4eb1ded8b314cff335c96b0f933862b
Carlos Abalde <cabalde@allenta.com>
2026-05-07 11:38:46 +0200

    Add 'tmp_dir' macro to VTCs

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,27 +3,27 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# Initializations.
 ROOT="$(dirname "$(realpath "$0")")"
+TEMP="$(mktemp -d)"
+trap 'rm -rf "$TEMP"' EXIT
 
-# VCL files are copied to a temporary location in order to apply some minimal
-# adjustments needed for testing.
-TMPDIR="$(mktemp -d)"
-trap 'rm -rf "$TMPDIR"' EXIT
-
-# Copy VCL files to a temporary location with these tweaks:
+# Copy VCL files to a temporary location and apply some tweaks:
 #   - Include 'akamai.vcl' (might be handy for some tests).
 #   - Use 'replication-disabled.vcl' as the replication option.
 #   - Activate calls to subroutines defined in VTCs for extra instrumentation.
-cp -r "$ROOT/../vcl"/. "$TMPDIR"/
+VCL_PATH="$TEMP/vcl"
+mkdir -p "$VCL_PATH"
+cp -r "$ROOT/../vcl"/. "$VCL_PATH"/
 sed -i \
     -e 's/^# \(include "akamai.vcl";\)/\1/' \
     -e 's/^\(include "replication-.*\.vcl"\);/# \1;/' \
     -e "s/^# \(include \"replication-disabled.vcl\";\)/\1/" \
-    "$TMPDIR/main.vcl"
+    "$VCL_PATH/main.vcl"
 sed -i \
     -e 's/^\s*# \(call vtc_.*;\)$/\1/' \
-    "$TMPDIR/main.vcl" \
-    "$TMPDIR/environment-local.vcl"
+    "$VCL_PATH/main.vcl" \
+    "$VCL_PATH/environment-local.vcl"
 
 # Discover VTC files to run.
 if [[ $# -gt 0 ]]; then
@@ -32,9 +32,13 @@ else
     mapfile -t VTCS < <(find "$ROOT" -name '*.vtc' -type f | sort)
 fi
 
-# Run VTC tests until the first failure is encountered.
+# Run VTC tests until the first failure is encountered. Use a separate temporary
+# directory for each test.
 for vtc in "${VTCS[@]}"; do
-    varnishtest -Dvcl_path="$TMPDIR" "$vtc"
+    TMP_PATH="$TEMP/tmp/$(basename "$vtc" .vtc)"
+    mkdir -p "$TMP_PATH"
+    varnishtest \
+        -Dvcl_path="$VCL_PATH" \
+        -Dtmp_path="$TMP_PATH" \
+        "$vtc"
 done
-
-
